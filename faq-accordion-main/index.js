da05be2e75afc859a0495d6e8e01ac03be716759
Taizun J <jafri.taizun.s@gmail.com>
2024-01-21 23:30:18 +0530

    Adding JS file

---
+++ faq-accordion-main/index.js
@@ -0,0 +1,12 @@
+
+// const accordian = document.querySelectorAll('.answer')
+
+const tr = document.querySelectorAll("answer")
+
+tr.forEach(
+    e =>{
+        e.addEventListener("click", ()=>{
+            console.log("help JS")
+        })
+    }
+)
